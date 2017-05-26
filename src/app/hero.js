84591d0c7ad349e4eac721611b2cefb5e79bce1f
marina-harlach <marina.garlach@logic-way.com>
2017-05-26 19:11:19 +0300

    Init

---
+++ src/app/hero.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Created by marina.garlach on 24.05.2017.
+ */
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
---
+++ src/app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by marina.garlach on 24.05.2017.
+ */
+export class Hero {
+    id: number;
+    name: string;
+}
